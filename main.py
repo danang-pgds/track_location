3675370ca984eb97050a8011c508fe5827e911ff
danang-pgds <danang.pgds@gmail.com>
2022-07-23 17:50:40 +0800

    Add multiple marker

--- main.py
+++ main.py
@@ -15,19 +15,27 @@ print(carrier.name_for_number(service_pro, "en"))
 
 from opencage.geocoder import OpenCageGeocode
 
-key = 'input your API key'
+key = 'ea4c8d1b27214a539d849f8b7122d292'
 
 geocoder = OpenCageGeocode(key)
 query = str(location)
 results =  geocoder.geocode(query)
-#print(results)
+# print(results)
 
 lat = results[0]['geometry']['lat']
 lng = results[0]['geometry']['lng']
 
 print(lat,lng)
 
-myMap = folium.Map(location=[lat,lng], zoom_start= 9)
-folium.Marker([lat,lng], popup=location).add_to(myMap)
+myMap = folium.Map(location=[lat,lng], zoom_start= 5)
+
+n = 0
+max_con = 0
+for x in results:
+    max_con = results[n]['confidence']
+    if(results[n]['confidence'] >= 5):
+        folium.Marker([results[n]['geometry']['lat'],results[n]['geometry']['lng']], popup=max_con).add_to(myMap)
+    n += 1
+print(n)
 
 myMap.save("mylocation.html")
